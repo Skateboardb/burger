c51bf8473dbda6e33dd10edf0c801414e9956f72
Branford Shaw <branford.shaw@gmail.com>
2020-01-27 17:13:46 -0500

    set up config component by creating connection.js file and formatting orm.js file

---
+++ config/orm.js
@@ -0,0 +1,15 @@
+var orm = {
+	selectAll: function() {
+		// selects all items in database
+	},
+
+	insertOne: function() {
+		// inserts a single entry into database
+	},
+
+	updateOne: function() {
+		// updates a single entry in database
+	}
+};
+
+module.exports = orm;
