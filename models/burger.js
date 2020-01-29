2fc4771abda1073aa19a7a672efab770abad1cfa
Branford Shaw <branford.shaw@gmail.com>
2020-01-29 12:30:15 -0500

    created code within burger.js that calls orm functions

--- models/burger.js
+++ models/burger.js
@@ -1,15 +1,37 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-	selectAll: function(cb) {
-		// calls orm.selectAll function
+	// calls orm.selectAll function
+	all: function(cb) {
+		orm.selectAll("burgers", function(res) {
+			cb(res);
+		});
 	},
 
-	insertOne: function() {
-		// calls orm.insertOne function
+	// calls orm.insertOne function
+	insert: function(vals, cb) {
+		orm.insertOne("burgers", ["burger_name"], vals, function(res) {
+			cb(res);
+		});
 	},
 
-	updateOne: function() {
-		// calls orm.updateOne function
+	// calls orm.updateOne function
+	update: function(colVals, condition, cb) {
+		colVals.date = new Date()
+			.toISOString()
+			.slice(0, 19)
+			.replace("T", " ");
+		orm.updateOne("burgers", colVals, condition, function(res) {
+			cb(res);
+		});
+	},
+
+	delete: function(condition, cb) {
+		orm.deleteOne("burgers", condition, function(res) {
+			cb(res);
+		});
 	}
 };
+
+// export functions for controller
+module.exports = burger;
