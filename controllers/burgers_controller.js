7265f677ae26ebe31d15647328f587f52db9bf86
Branford Shaw <branford.shaw@gmail.com>
2020-01-29 12:36:23 -0500

    added get call to burgers_controller to bring up index by default

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,4 +2,18 @@ var express = require("express");
 
 var burger = require("../models/burger.js");
 
+var router = express.Router();
+
+// Create all routes and set logic within to perform specific tasks
+
+// brings up index handlebars object on default page
+router.get("/", function(req, res) {
+	burger.all(function(data) {
+		var hbsObject = {
+			burgers: data
+		};
+		res.render("index", hbsObject);
+	});
+});
+
 module.exports = router;
