24ca0ab736be7d0d42984ac06bdefc9ed4420424
Branford Shaw <branford.shaw@gmail.com>
2020-01-29 15:28:47 -0500

    wrote jquery function to devour burger with ajax PUT request

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -21,4 +21,21 @@ $(function() {
 			$("[name=burger-name]").val("");
 		}
 	});
+	$(".devour-burger").on("click", function(event) {
+		var id = $(this).data("id");
+
+		var newState = {
+			devoured: true
+		};
+
+		// Send the PUT request.
+		$.ajax("/api/burgers/" + id, {
+			type: "PUT",
+			data: newState
+		}).then(function() {
+			// console.log("changed devour to", true);
+			// Reload the page to get the updated list
+			location.reload();
+		});
+	});
 });
